ee59b56a77d9bbb79b1cf56f3d1d3ef7027d6425
Liliia <kryva.liliia@gmail.com>
2022-10-14 11:35:26 +0300

    Updated Photos and Users components

--- src/components/Photos/Photos.jsx
+++ src/components/Photos/Photos.jsx
@@ -1,7 +1,26 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
 const Photos = () => {
+    const [photos, setPhotos] = useState([]);
+
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/photos")
+        .then((res) => res.json())
+        .then((data) => setPhotos(data));
+    }, []);
+
     return (
         <div>
             <h1>Photos</h1>
+            <ul>
+                {photos.map((photo) => (
+                    <Link key={photo.id} to={`/photos/${photo.id}`}>
+                        <li>{photo.title}</li>
+                        <img src={photo.url} alt="" />
+                    </Link>
+                ))}
+            </ul>
         </div>
     )
 }
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,7 +1,27 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
 const Users = () => {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/users")
+        .then((res) => res.json())
+        .then((data) => setUsers(data));
+    }, []);
+
     return (
         <div>
             <h1>Users</h1>
+            <ul>
+                {users.map((user) => (
+                    <Link key={user.id} to={`/users/${user.id}`}>
+                        <li>{user.name}</li>
+                        <p>{user.email}</p>
+                        <p>{user.phone}</p>
+                    </Link>
+                ))}
+            </ul>
         </div>
     )
 }
